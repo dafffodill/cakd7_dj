0696cc6f440943c3f035365c2e17431f12e01dc7
dafffodill <osmaanthus@gmail.com>
2022-08-04 17:11:50 +0900

    FBV로 블로그 포스트 목록 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,30 @@
+"""from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+
+class PostDetail(DetailView):
+    model = Post
+
+
+# Create your views here.
+
+"""
 from django.shortcuts import render
+from .models import Post
+
+def index(request):
+    posts = Post.objects.all().order_by('-pk')
+
+    return render(
+        request,
+        'blog/index.html',
+        {
+            'posts': posts,
+        }
+    )
 
 # Create your views here.
